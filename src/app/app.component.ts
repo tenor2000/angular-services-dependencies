e39977ef60b35763150d9054613b312e5d2af74b
tenor2000 <tenor2000@gmail.com>
2025-03-20 13:34:24 -0400

    created injected json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PeopleService } from '../shared/people.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'leadership';
+export class AppComponent implements OnInit {
+  peopleArray: any;
+
+  constructor(private peopleService: PeopleService) {}
+
+  getPeople() {
+    this.peopleService.getPeople().subscribe((people) => {
+      this.peopleArray = people;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPeople();
+  }
 }
